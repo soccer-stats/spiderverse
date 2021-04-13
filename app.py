7a39b2830499a6811802d5633fa3dea379596961
rfedotov <rsfedotov@gmail.com>
2021-04-13 14:45:19 -0700

    Add MF and DF filtering support

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ st.title('Polar Bear')
 # st.write("Soccer player performance visualization")
 st.write("Created by [Roaming Playmaker](https://t.me/Ivan_xG) и [Футбол в цифрах](https://t.me/markstats)")
 
-template = st.sidebar.selectbox('Template', ["forwards"])
+template = st.sidebar.selectbox('Position', ["Forwards", "Midfielders", "Defenders"])
 template_to_position_mapping = {
-    "forwards": {"filter": "FW", "default_player": "Lionel Messi"},
+    "Forwards": {"filter": "FW", "default_player": "Lionel Messi"},
+    "Midfielders": {"filter": "MF", "default_player": "Kevin De Bruyne"},
+    "Defenders": {"filter": "DF", "default_player": "Juan Cuadrado"},
 }
 df = data_loader.load_data(template_to_position_mapping[template]["filter"])
 player_names = df['Player'].unique().tolist()

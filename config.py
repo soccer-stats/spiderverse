69e0bf3aba48169f1f04ed15deda22c11c6ca37d
ivanxG <i.s-off@ya.ru>
2021-04-24 09:12:06 +0300

    updated data

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE_PATH, "data")
-DATE_UPDATE = "2021-04-21"
+DATE_UPDATE = "2021-04-24"
 
 template_to_position_mapping = {
     "Forwards": {"filter": "FW", "default_player": "Lionel Messi"},

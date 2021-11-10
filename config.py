facedb89c5c8406ea75bcc5e8a20e345e9a302dd
ivanxG <i.s-off@ya.ru>
2021-11-10 09:39:21 +0300

    Revert "updated data"
    This reverts commit 1ab0029b

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 BASE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE_PATH, "data")
-DATE_UPDATE = "2021-11-02"
+DATE_UPDATE = "2021-11-10"
 
 template_to_position_mapping = {
     "2021-2022": {

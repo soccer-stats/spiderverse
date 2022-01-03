f431a8ac8ac8da04417fa80a39eebb644cafe05d
ivanxG <i.s-off@ya.ru>
2022-01-04 00:02:50 +0300

    Revert "updated data"
    This reverts commit 1ab0029b

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 BASE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE_PATH, "data")
-DATE_UPDATE = "2022-01-04"
+DATE_UPDATE = "2022-01-03"
 
 template_to_position_mapping = {
     "2021-2022": {

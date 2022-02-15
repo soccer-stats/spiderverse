2a96064fd80a837d041e2f07fda527f1ff56004b
ivanxG <i.s-off@ya.ru>
2022-02-16 00:51:53 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	template/template_service.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit_analytics
 from service import data_loader
 from service import polar_plot_service
 import strings
@@ -20,25 +19,24 @@ def prepare_and_read_input_params(season_label_name, player_label_name, default_
     return season, players_df, name
 
 
-with streamlit_analytics.track():
-    st.title(strings.get_string("app_title"))
-    st.write(strings.get_string("app_credentials") + ", " + strings.get_string("donate"))
+st.title(strings.get_string("app_title"))
+st.write(strings.get_string("app_credentials") + ", " + strings.get_string("donate"))
 
-    mode = st.sidebar.selectbox('Mode', ["Single player", "Compare players"])
-    template = st.sidebar.selectbox('Position', ["Forwards", "Midfielders", "Defenders"])
+mode = st.sidebar.selectbox('Mode', ["Single player", "Compare players"])
+template = st.sidebar.selectbox('Position', ["Forwards", "Midfielders", "Defenders"])
 
-    if mode == "Single player":
-        season, df, player_name = prepare_and_read_input_params('Season', 'Player')
-        st.pyplot(polar_plot_service.draw_polar(df, player_name, template, season))
-    else:
-        season1, df1, player1_name = prepare_and_read_input_params(
-            'Season 1', 'Player 1')
-        season2, df2, player2_name = prepare_and_read_input_params(
-            'Season 2', 'Player 2', 'default_compare_player')
-        df = df1 if season1 == season2 else df1.append(df2)
-        st.pyplot(polar_plot_service.draw_polar2(
-            df, player1_name, player2_name, template, season1, season2))
+if mode == "Single player":
+    season, df, player_name = prepare_and_read_input_params('Season', 'Player')
+    st.pyplot(polar_plot_service.draw_polar(df, player_name, template, season))
+else:
+    season1, df1, player1_name = prepare_and_read_input_params(
+        'Season 1', 'Player 1')
+    season2, df2, player2_name = prepare_and_read_input_params(
+        'Season 2', 'Player 2', 'default_compare_player')
+    df = df1 if season1 == season2 else df1.append(df2)
+    st.pyplot(polar_plot_service.draw_polar2(
+        df, player1_name, player2_name, template, season1, season2))
 
-    st.write("'Pass - xPass completion %' - difference between ACTUAL and EXPECTED Pass Completion")
-    st.write("Expected Pass Completion calculated based on various passing metrics using linear regression")
-    st.write("The height of the dueling sectors is calculated using Bayesian estimator, which builds on successful actions throughout the season")
+st.write("'Pass - xPass completion %' - difference between ACTUAL and EXPECTED Pass Completion")
+st.write("Expected Pass Completion calculated based on various passing metrics using linear regression")
+st.write("The height of the dueling sectors is calculated using Bayesian estimator, which builds on successful actions throughout the season")

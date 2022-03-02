cee84001e146ecd195cdae4f3f2711d5c4a330e6
ivanxG <i.s-off@ya.ru>
2022-03-02 16:59:06 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	template/template_service.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 BASE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE_PATH, "data")
-DATE_UPDATE = "2022-02-16"
+DATE_UPDATE = "2022-03-02"
 
 template_to_position_mapping = {
     "2021-2022": {
